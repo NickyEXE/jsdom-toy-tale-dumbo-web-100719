7146b7eb12955cf092eee1bab9c2455bbf63cc31
NickyEXE <dovern42@gmail.com>
2020-03-17 10:26:34 -0400

    finished adapter refactor and cleaned up

--- src/adapter.js
+++ src/adapter.js
@@ -1,10 +1,19 @@
 adapter = {
     index: () => fetch('http://localhost:3000/toys').then(res => res.json()),
+    
     like: (id, newLikeCount) => fetch(`http://localhost:3000/toys/${id}`, {
             method: 'PATCH', // or 'PUT'
             headers: {
               'Content-Type': 'application/json',
             },
             body: JSON.stringify({"likes": newLikeCount}),
-          }).then(res => res.json())
+          }).then(res => res.json()),
+
+    addNewToy: (toy) => fetch('http://localhost:3000/toys', {
+        method: 'POST', // or 'PUT'
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(toy),
+      }).then(res => res.json())
 }
--- src/index.js
+++ src/index.js
@@ -15,72 +15,66 @@ addBtn.addEventListener('click', () => {
 
 adapter.index().then(toys => toys.forEach(addToyToDOM))
 
-// const addToyToDOM = (toy) => {
-//   toyContainer.innerHTML+= `
-//   <div class="card" data-id=${toy.id}>
-//     <h2>${toy.name}</h2>
-//     <img src=${toy.image} class="toy-avatar" alt=${toy.name} />
-//     <p>${toy.likes} Likes </p>
-//     <button class="like-btn">Like <3</button>
-//   </div>
-//   `
-// }
-
-// const handleToyClick = (e) => {
-//   if (e.target.matches("button")){
-//     likeToy(e.target.closest(".card"))
-//   }
-// }
-
-// toyContainer.addEventListener("click", handleToyClick)
-
-// const likeToy = (toyDiv) => {
-//   const id = toyDiv.dataset.id
-//   const likesP = toyDiv.querySelector("p")
-//   let numLikes = likesP.innerText.slice(0, -6)
-//   numLikes ++
-//   adapter.like(id, numLikes)
-//   .then(res => likesP.innerText = res.likes + " Likes")
-// }
-
+// Event delegation
 
 const addToyToDOM = (toy) => {
-  const div = document.createElement("div")
-  div.className = "card"
-  // children
-  const h2 = document.createElement("h2")
-  h2.innerText = toy.name
-  const img = document.createElement("img")
-  img.src = toy.image
-  img.alt = toy.name
-  img.className = "toy-avatar"
-  const p = document.createElement("p")
-  p.innerText = toy.likes + " Likes"
-  const button = document.createElement("button")
-  button.className = "like-btn"
-  button.innerText = "Like <3"
-  div.append(h2, img, p, button)
-  toyContainer.appendChild(div)
-  button.addEventListener("click", () => {
-    adapter.like(toy.id, toy.likes+1)
-    .then(newToyData => {
-      toy = newToyData
-      p.innerText = toy.likes + " Likes"
-    })
-  })
+  toyContainer.innerHTML+= `
+  <div class="card" data-id=${toy.id}>
+    <h2>${toy.name}</h2>
+    <img src=${toy.image} class="toy-avatar" alt=${toy.name} />
+    <p>${toy.likes} Likes </p>
+    <button class="like-btn">Like <3</button>
+  </div>
+  `
 }
 
-const addNewToy = toy => {
-  fetch('http://localhost:3000/toys', {
-    method: 'POST', // or 'PUT'
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(toy),
-  })
-  .then(res => res.json())
-  .then(addToyToDOM)
+const handleToyClick = (e) => {
+  if (e.target.matches("button")){
+    likeToy(e.target.closest(".card"))
+  }
 }
+
+toyContainer.addEventListener("click", handleToyClick)
+
+const likeToy = (toyDiv) => {
+  const id = toyDiv.dataset.id
+  const likesP = toyDiv.querySelector("p")
+  let numLikes = likesP.innerText.slice(0, -6)
+  numLikes ++
+  adapter.like(id, numLikes)
+  .then(res => likesP.innerText = res.likes + " Likes")
+}
+
+// -------
+
+// No event delegation
+// const addToyToDOM = (toy) => {
+//   const div = document.createElement("div")
+//   div.className = "card"
+//   const h2 = document.createElement("h2")
+//   h2.innerText = toy.name
+//   const img = document.createElement("img")
+//   img.src = toy.image
+//   img.alt = toy.name
+//   img.className = "toy-avatar"
+//   const p = document.createElement("p")
+//   p.innerText = toy.likes + " Likes"
+//   const button = document.createElement("button")
+//   button.className = "like-btn"
+//   button.innerText = "Like <3"
+//   div.append(h2, img, p, button)
+//   toyContainer.appendChild(div)
+//   button.addEventListener("click", () => {
+//     adapter.like(toy.id, toy.likes+1)
+//       .then(newToyData => {
+//         toy = newToyData
+//         p.innerText = toy.likes + " Likes"
+//     })
+//   })
+// }
+// ---------
+
+const addNewToy = toy => adapter.addNewToy(toy).then(addToyToDOM)
 
 const toyFormForm = document.querySelector(".add-toy-form")
 
